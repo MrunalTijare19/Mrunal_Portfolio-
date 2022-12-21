ef46c06d55eff6183cc9c5b40a5d0f9eca2c1bc8
Mrunal <lanurm123@gmail.com>
2022-12-22 00:48:49 +0530

    modified

--- MrunalPortfolio/settings.py
+++ MrunalPortfolio/settings.py
@@ -74,12 +74,12 @@ WSGI_APPLICATION = 'MrunalPortfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+}
 
 
 # Password validation
@@ -125,13 +125,16 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 # Manually added
-
+# importing os module
 import os
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
+
 # STATICFILES_DIRS = [
 #     BASE_DIR / "static"
 # ]
 
-STATICFILES_DIRS = os.path.join(BASE_DIR / "static"),
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
 
 
